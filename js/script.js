faf06f15e51832c9858140c30e9c57aaabd0b4a5
Sylwia <sylwia_24_24@wp.pl>
2019-07-04 20:09:16 +0200

    Add first step game

---
+++ js/script.js
@@ -0,0 +1,11 @@
+function printMessage(msg){
+	var div = document.createElement('div');
+	div.innerHTML = msg;
+	document.getElementById('messages').appendChild(div);
+}
+
+function clearMessages(){
+	document.getElementById('messages').innerHTML = '';
+}
+let playerMove = 'papier';
+printMessage('Zagrałem ' + playerMove + '! Jeśli Twój ruch to kamień, to wygrałem');
