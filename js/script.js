6e54366590f504141f1c47f9765ccea256486350
Sylwia <sylwia_24_24@wp.pl>
2019-07-06 14:30:17 +0200

    Add last commit

--- js/script.js
+++ js/script.js
@@ -33,3 +33,44 @@ else if(playerInput == '3'){
 	playerMove ='nożyce';
 }
 printMessage('Twój ruch to: ' + playerMove);
+
+if( computerMove == 'kamień' && playerMove == 'papier'){
+  printMessage('Ty wygrywasz');
+}
+else if(computerMove == 'kamień' && playerMove == 'nożyce'){
+	printMessage('Przegrałeś');
+}
+else if(computerMove == 'kamień' && playerMove == 'kamień'){
+	printMessage('Remis');
+}
+else if(computerMove == 'kamień' && playerMove == 'nieznany ruch'){
+	printMessage('Spróbuj jeszcze raz wybrać liczbę 1, 2 lub 3');
+}
+
+if( computerMove == 'nożyce' && playerMove == 'kamień'){
+  printMessage('Ty wygrywasz');
+}
+else if(computerMove == 'nożyce' && playerMove == 'kamień'){
+	printMessage('Przegrałeś');
+}
+else if(computerMove == 'nożyce' && playerMove == 'nożyce'){
+	printMessage('Remis');
+}
+else if(computerMove == 'nożyce' && playerMove == 'nieznany ruch'){
+	printMessage('Spróbuj jeszcze raz wybrać liczbę 1, 2 lub 3');
+}
+if( computerMove == 'papier' && playerMove == 'nożyce'){
+  printMessage('Ty wygrywasz');
+}
+else if(computerMove == 'papier' && playerMove == 'kamień'){
+	printMessage('Przegrałeś');
+}
+else if(computerMove == 'papier' && playerMove == 'papier'){
+	printMessage('Remis');
+}
+else if(computerMove == 'papier' && playerMove == 'nieznany ruch'){
+	printMessage('Spróbuj jeszcze raz wybrać liczbę 1, 2 lub 3');
+}
+else if(computerMove == 'nieznany ruch' && playerMove == 'papier'){
+	printMessage('Cos poszło nie tak, spróbuj jeszcze raz');
+}
